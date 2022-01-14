168eabaeb33449f93304f7616f99d484cd340ddb
Daniel Messias <daniel.messias17@gmail.com>
2022-01-14 12:31:34 -0300

    implementação

--- exercicio-coder-springboot/src/main/java/br/com/daniel/exercicio/coder/springboot/controllers/ProdutosController.java
+++ exercicio-coder-springboot/src/main/java/br/com/daniel/exercicio/coder/springboot/controllers/ProdutosController.java
@@ -1,11 +1,16 @@
 package br.com.daniel.exercicio.coder.springboot.controllers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +20,10 @@ import br.com.daniel.exercicio.coder.springboot.models.repository.ProdutoReposit
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutosController {
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 //
 //	@PostMapping
 //	public @ResponseBody Produto novoProduto(@RequestParam String nome, @RequestParam double preco,@RequestParam double desconto) {   
@@ -27,14 +32,40 @@ public class ProdutosController {
 //		return produto;
 //
 //	}
-	
-	
-	// outra forma de escrita do metodo acima
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@Valid Produto produto) {   
+
+	// outra forma de escrita do metodo acima (Create)
+
+// @PostMapping 
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.PUT })
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
 
 	}
 
+	// obter todos os produtos do banco de dados ( Read)
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+
+	// obter produto por ID
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+
+	// update do produto
+	@PutMapping
+	public Produto alterarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
+	}
+
+	// excluir produto
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+
+	}
 }
